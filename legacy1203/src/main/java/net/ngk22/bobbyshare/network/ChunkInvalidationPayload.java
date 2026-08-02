885b8a2c876d570574723218eae0865ef396f40c
nikitagk22 <golik.niki@gmail.com>
2026-08-02 23:23:50 +0300

    Remove redundant 1.20.3 port

--- legacy1203/src/main/java/net/ngk22/bobbyshare/network/ChunkInvalidationPayload.java
+++
@@ -1,7 +0,0 @@
-package net.ngk22.bobbyshare.network;
-
-import net.minecraft.util.Identifier;
-
-public record ChunkInvalidationPayload(int x, int z) {
-    public static final Identifier ID = new Identifier("bobbyshare", "chunk_invalidation");
-}
--- legacy1203/src/main/java/net/ngk22/bobbyshare/network/ChunkRequestPayload.java
+++
@@ -1,7 +0,0 @@
-package net.ngk22.bobbyshare.network;
-
-import net.minecraft.util.Identifier;
-
-public record ChunkRequestPayload(int x, int z) {
-    public static final Identifier ID = new Identifier("bobbyshare", "chunk_request");
-}
