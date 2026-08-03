c6f1620b44f3e751fbbe2d07619c65ee84625b70
nikitagk22 <golik.niki@gmail.com>
2026-08-03 12:53:46 +0300

    Release v1.3.1: Fix duplicate payload registration, embed mod icon, bump versions

--- legacy12111/src/main/java/net/ngk22/bobbyshare/client/BobbyShareClient.java
+++ legacy12111/src/main/java/net/ngk22/bobbyshare/client/BobbyShareClient.java
@@ -3,16 +3,12 @@ package net.ngk22.bobbyshare.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.ngk22.bobbyshare.network.ChunkResponsePayload;
 import net.ngk22.bobbyshare.network.ChunkInvalidationPayload;
 import net.ngk22.bobbyshare.network.ChunkRequestPayload;
 
 public class BobbyShareClient implements ClientModInitializer {
     @Override public void onInitializeClient() {
-        PayloadTypeRegistry.playC2S().register(ChunkRequestPayload.ID, ChunkRequestPayload.CODEC);
-        PayloadTypeRegistry.playS2C().register(ChunkResponsePayload.ID, ChunkResponsePayload.CODEC);
-        PayloadTypeRegistry.playS2C().register(ChunkInvalidationPayload.ID, ChunkInvalidationPayload.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(ChunkResponsePayload.ID, (payload, context) -> {
             context.client().execute(() -> ClientChunkRequester.handleResponse(payload));
         });
